c7d319499eff18146650b306e2c56ea363ad74aa
katherin cucunuba <66915247+Katherine9309@users.noreply.github.com>
2023-04-15 01:40:12 -0500

    Second point of the second task

---
+++ PointTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Task 2 - Point 2");
+Console.WriteLine("Write the  ");
+string number = Console.ReadLine();
+
+SwapsNumber(number);
+Console.ReadKey();
+
+
+//Methods
+
+static void  SwapsNumber(string array) {
+
+    
+    string [] splitArray = array.Split(",");
+    int length = splitArray.Length - 1;
+
+    for (int i=0;i<splitArray.Length;i++) {
+
+        if (i != (splitArray.Length-1))
+            Console.Write(splitArray[length] + ",");
+        else
+            Console.Write(splitArray[length]);
+
+        length--;
+    }
+}
