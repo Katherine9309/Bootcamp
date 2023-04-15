b8c4919230c580c721ec6b8c8f29f993ee1d040a
katherin cucunuba <66915247+Katherine9309@users.noreply.github.com>
2023-04-15 01:39:06 -0500

    Point one of Second task

---
+++ PointOne/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//string phrase;
+Console.WriteLine("Task_2 Point 1");
+Console.WriteLine("Write the phrase to analyze: ");
+string phrase = Console.ReadLine();
+//string phrase = "Hgggh, jsj lkks, a lil-lil – lop";
+phrase = phrase.Replace(',', ' ').Replace('–', ' ');
+var words = phrase.Split( ' ');
+string finalPhrase = default;
+// Split the string
+
+for (int i=0; i < words.Length; i++)
+{
+    words[i] = words[i].Trim();
+
+    if (!(string.Equals(words[i], ""))) {
+        var result = IsPalindrome(words[i]) ? " - palindrome, " : " - not palindrome, ";
+        finalPhrase = finalPhrase + words[i] + result;
+    }  
+}
+Console.Write(finalPhrase);
+Console.ReadKey();
+
+
+//methods
+
+bool IsPalindrome(string str)
+{
+    char[] array = str.ToCharArray();
+    int length = array.Length - 1;
+    Boolean palindrome = true;
+    for (int i = 0; i <= length; i++)
+    {
+        // para leter case StringComparison.OrdinalIgnoreCase
+        if (string.Compare(array[i].ToString(), array[length].ToString(),true) != 0)
+        {
+            palindrome = false;
+            break;
+        }
+        else
+        {
+            length--;
+        }
+
+    }
+    return palindrome;
+}
