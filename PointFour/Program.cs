16670ad93c8a4c1615fd7df21962fa237e4a8ec9
Katherin Cucunuba <katherin_cucunuba@hotmail.com>
2023-05-03 23:23:40 -0500

    logic fixed

--- PointFour/Program.cs
+++ PointFour/Program.cs
@@ -2,120 +2,181 @@
 using System;
 using System.Net.NetworkInformation;
 
-   
-Console.WriteLine("Task 2, point 4");
-Console.WriteLine();
 
-
-char[,] field = new char[6, 10] { { '5','+', '-', '+', '-', '-', '-', '-', '-', '+' }, { '4','+', '-', '+', '-', '-', '+', '-', '-', '+' }, { '3','-', '-', '+', '-', '-', '-', '-', '-', '+' }, { '2','+', '-', '+', '-', '-', '+', '-', '-', '+' }, { '1','-', '-', '+', '-', '-', '+', '-', '-', '+' } ,{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' } };
-bool statusGame = false;
-char[,] future = new char[6, 10];
-char[,] pastfield = new char[6, 10];
-for (int x = 0; x < field.GetLength(0)-1; x++)
+internal class Program
 {
-    for (int y = 1; y < field.GetLength(1); y++)
-    {
-       Console.ReadKey();
-       Console.Clear();
-       Console.WriteLine("Start Field");
-       DrawField(field);
-       pastfield = (char[,])field.Clone();
-       future = FutureField(field,x,y);
-       Console.WriteLine("Future Field");
-       DrawField(future);
     
-        statusGame = status(pastfield, future);
-        if (statusGame)
-        {
-            break;
-        }
-    }
-    if (statusGame)
+    
+    
+    
+    static void Main(string[] args)
     {
-        Console.WriteLine("GameOver");
-        break;
-    }
-    else { field = future; }
-}
+        bool statusGame = false;
+        char[,] field = new char[6, 10] {
+        //{ '5','+', '-', '+', '-', '-', '-', '-', '-', '+' },
+        //{ '4','+', '-', '+', '-', '-', '+', '-', '-', '+' }, 
+        //{ '3','-', '-', '+', '-', '-', '-', '-', '-', '+' }, 
+        //{ '2','+', '-', '+', '-', '-', '+', '-', '-', '+' },
+        //{ '1','-', '-', '+', '-', '-', '+', '-', '-', '+' }, 
+        //{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' } };
 
-Console.ReadKey();
-
-
-
-//Method to print
-
-void DrawField(char[,] gridToPrint)
-{
-    for (int i = 0; i < gridToPrint.GetLength(0); i++)
-    {
-        for (int j = 0; j < gridToPrint.GetLength(1); j++)
-        { 
-            Console.Write(gridToPrint[i, j] + "\t");
-        }
+        { '5','-', '-', '-', '-', '-', '-', '-', '-', '-' },
+        { '4','-', '-', '+', '-', '-', '-', '-', '-', '-' },
+        { '3','-', '-', '+', '-', '-', '-', '-', '-', '-' },
+        { '2','-', '-', '+', '-', '-', '-', '-', '-', '-' },
+        { '1','-', '-', '-', '-', '-', '-', '-', '-', '-' },
+        { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' } };
+       
+        char[,] pastfield = new char[6, 10];
+        Console.WriteLine("Task 2, point 4");
         Console.WriteLine();
-    }
-}
+       
 
-
-//Method to calculate the next generation
-char[,] FutureField(char[,] currenField, int x,int y)
-{
-    int neighbors = CheckNeigbors(currenField, x,y);
-    currenField[x, y]= ModifiedField(neighbors, currenField[x, y]);
-    return currenField;
-}
-
-//Method check neigbors
-int CheckNeigbors(char[,] currenField, int x,int y)
-{
-    // int[,] neigbors = new int[];
-
-    int neigbors = 0;
-    for (int xn= -1; xn <=1; xn++)
-    {
-        for (int yn = -1; yn <=1; yn++)
+        while (Console.ReadKey().Key == ConsoleKey.Enter)
         {
-            if ((!(xn == 0 && yn == 0))&&(y+yn)>0&& (x+xn)>=0&& (y + yn)<currenField.GetLength(1) && (x + xn)<currenField.GetLength(0)-1) {
-                 neigbors += (currenField[x+xn,y+yn]== '+') ? 1 : 0;
-                
+            Console.Clear();
+            char[,] future = new char[6, 10];
+            Console.WriteLine("Start Field");
+           
+             DrawField(field);
+           // future = FutureField(pastfield);
+            
+
+            Console.WriteLine("Future Field");
+
+            ///////////
+            for (int x = 0; x < field.GetLength(0) - 1; x++)
+            {
+                for (int y = 1; y < field.GetLength(1); y++)
+                {
+
+                    int neighbors = CheckNeigbors(field, x, y);
+                    future[x, y] = ModifiedField(neighbors, field[x, y]);
+
+                }
             }
+            ////////////
+            DrawField(future);
+
+            Console.ReadKey();
+            Console.WriteLine();
+            statusGame = status(field, future);
+            if (statusGame)
+            {
+                break;
+            }
+
+            if (statusGame)
+            {
+                Console.WriteLine("GameOver");
+                break;
+            }
+            else { field = future; 
+            
+            }
+
         }
-
     }
-    Console.WriteLine("neigbors " + neigbors);  
-    return neigbors;
-}
 
-char ModifiedField(int neigbors,char character) {
+        //Method to print
 
-    if (character == '+')
+    public static void DrawField(char[,] gridToPrint)
     {
-        if (neigbors <= 1 || neigbors >= 4) { return '-'; }
-        else { return '+'; }
-    }
-    else
-    {
-        if (neigbors == 3) { return '+'; }
-        else { return '-'; }
-    }
-}
-
-bool status(char[,] presentField, char[,] FutureField)
-{
-    int count=0,count2 = 0;
-    for (int x = 0; x < field.GetLength(0) - 1; x++)
-    {
-        for (int y = 1; y < field.GetLength(1); y++)
+        for (int i = 0; i < gridToPrint.GetLength(0); i++)
         {
-            count += (FutureField[x , y] == '+') ? 1 : 0;
-            count2 += (FutureField[x, y] == presentField[x,y]) ? 0 : 1;
 
+            for (int j = 0; j < gridToPrint.GetLength(1); j++)
+            {
+                if (j == 0) { Console.Write((gridToPrint.GetLength(0) - i-1) + "\t"); }
+
+                else if (i == 5)
+                        Console.Write(j + "\t");
+
+                    else
+                    Console.Write(gridToPrint[i, j] + "\t");
+            }
+            Console.WriteLine();
         }
-
     }
     
-    Console.WriteLine("cont: " + count);
-    Console.WriteLine("cont2: " + count2);
-    if (count > 0 && count2 >0) { return false; }
-    else { return true; }
+
+
+    //Method to calculate the next generation
+    static char[,] FutureField(char[,] currenField)
+        {
+
+            for (int x = 0; x < currenField.GetLength(0) - 1; x++)
+            {
+                for (int y = 1; y < currenField.GetLength(1); y++)
+                {
+
+                    int neighbors = CheckNeigbors(currenField, x, y);
+                    currenField[x, y] = ModifiedField(neighbors, currenField[x, y]);
+                    
+                }
+            }
+        return currenField;
+    }
+
+        //Method check neigbors
+    static int CheckNeigbors(char[,] currenField, int x, int y)
+    {
+        // int[,] neigbors = new int[];
+
+        int neigbors = 0;
+        for (int xn = -1; xn <= 1; xn++)
+        {
+            for (int yn = -1; yn <= 1; yn++)
+            {
+                if (!(xn == 0 && yn == 0) && y + yn > 0 && x + xn >= 0 && y + yn < currenField.GetLength(1) && x + xn < currenField.GetLength(0) - 1)
+                {
+                    neigbors += currenField[x + xn, y + yn] == '+' ? 1 : 0;
+
+                }
+            }
+
+        }
+        // Console.WriteLine("neigbors " + neigbors);  
+        return neigbors;
+    }
+
+       static char ModifiedField(int neigbors, char character)
+        {
+
+            if (character == '+')
+            {
+
+                if (neigbors <= 1 || neigbors >= 4) { return '-'; }
+                else { return '+'; }
+            }
+            else if (character == '-' && neigbors == 3)
+            {
+                return '+';
+
+            }
+            else { return '-'; }
+        
+    }
+
+
+        static bool status(char[,] presentField, char[,] FutureField)
+        {
+            int count = 0;
+            int count2 = 0;
+            for (int x = 0; x < presentField.GetLength(0) - 1; x++)
+            {
+                for (int y = 1; y < presentField.GetLength(1); y++)
+                {
+                    count += FutureField[x, y] == '+' ? 1 : 0;
+                    count2 += (FutureField[x, y] == presentField[x, y]) ? 0 : 1;
+
+                }
+
+            }
+
+            Console.WriteLine("cont: " + count);
+            Console.WriteLine("cont2: " + count2);
+            if (count > 0 || count2 > 0) { return false; }
+            else { return true; }
+        }
 }
